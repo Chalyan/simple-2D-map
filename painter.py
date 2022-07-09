c0e17c00a1f514b004b690371ac982e06857a631
Phaze <gorchalyan@gmail.com>
2022-07-10 00:26:12 +0400

    [REFACTOR] Refactored setting of axises

--- painter.py
+++ painter.py
@@ -3,5 +3,5 @@ import board as brd
 
 class painter:
     
-    def paint(paintable_board:brd.board_map, x_ccordinate:int, y_coordinate:int, point_style):
-        paintable_board.map[x_ccordinate][y_coordinate] = point_style
+    def paint(paintable_board:brd.board_map, x_ccordinate:int, y_coordinate:int, point_style:str):
+        paintable_board.map[y_coordinate][x_ccordinate] = point_style
